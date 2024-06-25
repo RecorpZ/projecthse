3f1c804aab59e76ac6025c9f8f5151d48d9d26f4
RecorpZ <stepler_27@mail.ru>
2024-06-25 17:57:42 +0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 
 
 export const Home = ( ) => { 
+const role = localStorage.getItem('Role');
+const navigate = useNavigate(); 
 
+if (role == "student"){
+  setTimeout(() => {
+    navigate('/student',{ replace: true });
+    window.location.reload();
+    }, 500);
+}
+if (role == "teacher"){
+  setTimeout(() => {
+    navigate('/',{ replace: true });
+    window.location.reload();
+    }, 500);
+}
 
     return (
       <div>
